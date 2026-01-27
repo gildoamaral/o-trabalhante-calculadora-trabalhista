1b23cbf09b43d0ce8c75ca712e8bbf7a0a722eb8
Marco Antonio Gil <gildo_marco@hotmail.com>
2026-01-27 17:34:26 -0300

    minor bug fix

---
+++ src/app/decimo-terceiro/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const decimoTerceiro = () => {
+  return (
+    <div>decimoTerceiro</div>
+  )
+}
+
+export default decimoTerceiro
---
+++ src/app/recisao/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const recisao = () => {
+  return (
+    <div>recisao</div>
+  )
+}
+
+export default recisao
